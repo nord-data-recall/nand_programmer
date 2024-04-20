6d01f7704378be5ee683e9b746e36ad5bc074ddd
nord data recall <162032740+nord-data-recall@users.noreply.github.com>
2024-04-20 09:55:34 +0200

    shortening usb descriptors

--- firmware/usb_cdc/usb_desc.c
+++ firmware/usb_cdc/usb_desc.c
@@ -149,9 +149,8 @@ const uint8_t Virtual_Com_Port_StringVendor[VIRTUAL_COM_PORT_SIZ_STRING_VENDOR] 
     VIRTUAL_COM_PORT_SIZ_STRING_VENDOR,     /* Size of Vendor string */
     USB_STRING_DESCRIPTOR_TYPE,             /* bDescriptorType*/
     /* Manufacturer: "STMicroelectronics" */
-    'S', 0, 'T', 0, 'M', 0, 'i', 0, 'c', 0, 'r', 0, 'o', 0, 'e', 0,
-    'l', 0, 'e', 0, 'c', 0, 't', 0, 'r', 0, 'o', 0, 'n', 0, 'i', 0,
-    'c', 0, 's', 0
+    'N', 0, 'A', 0, 'N', 0, 'D', 0, 'O', 0, ' ', 0, 'D', 0, 'e', 0,
+    'v', 0, 'e', 0, 'l', 0, 'o', 0, 'p', 0, 'e', 0, 'r', 0, 's', 0
   };
 
 const uint8_t Virtual_Com_Port_StringProduct[VIRTUAL_COM_PORT_SIZ_STRING_PRODUCT] =
@@ -159,9 +158,8 @@ const uint8_t Virtual_Com_Port_StringProduct[VIRTUAL_COM_PORT_SIZ_STRING_PRODUCT
     VIRTUAL_COM_PORT_SIZ_STRING_PRODUCT,          /* bLength */
     USB_STRING_DESCRIPTOR_TYPE,        /* bDescriptorType */
     /* Product name: "STM32 Virtual COM Port" */
-    'S', 0, 'T', 0, 'M', 0, '3', 0, '2', 0, ' ', 0, 'V', 0, 'i', 0,
-    'r', 0, 't', 0, 'u', 0, 'a', 0, 'l', 0, ' ', 0, 'C', 0, 'O', 0,
-    'M', 0, ' ', 0, 'P', 0, 'o', 0, 'r', 0, 't', 0, ' ', 0, ' ', 0
+    'N', 0, 'A', 0, 'N', 0, 'D', 0, 'O', 0, ' ', 0, '3', 0, '.', 0, 
+    '5', 0
   };
 
 uint8_t Virtual_Com_Port_StringSerial[VIRTUAL_COM_PORT_SIZ_STRING_SERIAL] =
